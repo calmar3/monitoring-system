c0656faf20cc8efd59c07f52879d6bd3c19db747
calmar91 <m_calzetta@live.com>
2017-03-22 17:19:09 +0100

    write and read JSON from kafka

--- src/main/java/org/apache/flink/quickstart/LampSchema.java
+++ src/main/java/org/apache/flink/quickstart/LampSchema.java
@@ -1,9 +1,13 @@
 package org.apache.flink.quickstart;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.streaming.util.serialization.DeserializationSchema;
 import org.apache.flink.streaming.util.serialization.SerializationSchema;
+
+import java.io.IOException;
 
 /**
  * Created by maurizio on 21/03/17.
@@ -12,14 +16,40 @@ import org.apache.flink.streaming.util.serialization.SerializationSchema;
  */
 public class LampSchema implements DeserializationSchema<Lamp>, SerializationSchema<Lamp> {
 
+    public ObjectMapper mapper;
+
     @Override
     public byte[] serialize(Lamp element) {
-        return element.toString().getBytes();
+
+        this.mapper = new ObjectMapper();
+
+        String jsonInString = new String("");
+        try {
+            jsonInString = mapper.writeValueAsString(element);
+            System.out.println(jsonInString);
+            return jsonInString.getBytes();
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return jsonInString.getBytes();
+        }
+
     }
 
     @Override
     public Lamp deserialize(byte[] message) {
-        return Lamp.fromString(new String(message));
+
+        String jsonInString = new String(message);
+        this.mapper = new ObjectMapper();
+        Lamp lamp = new Lamp();
+        try {
+            lamp = this.mapper.readValue(jsonInString, Lamp.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lamp;
     }
 
     @Override
